86ab034effec4d4956e25eec60a668888d422419
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-01-17 17:04:49 -0600

    agregndo el objeto color con el objeto blue con 3 tonalidades

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,13 @@ const config: Config = {
       gridTemplateColumns: {
         '13': 'repeat(13, minmax(0, 1fr))',
       },
+      colors: {
+        blue: {
+          400: '#2589FE',
+          500: '#0070F3',
+          600: '#2F6FEB'
+        }
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
