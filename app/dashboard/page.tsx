220146ea1b7fedc1d077dcefea2b329b4fe8fe26
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-01-17 23:45:23 -0600

    agregando el fetchLtestInvoices y el component LatestInvoices

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,13 @@
-import { fetchRevenue } from "../lib/data"
+import { fetchLatestInvoices, fetchRevenue } from "../lib/data"
+import LatestInvoices from "../ui/dashboard/latest-invoices"
 import RevenueChart from "../ui/dashboard/revenue-chart"
 import { lusitana } from "../ui/fonts"
 
 export default async function Page() {
 
   const revenue = await fetchRevenue()
-  console.log(revenue)
+  const latestInvoices = await fetchLatestInvoices()
+  console.log(latestInvoices)
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -16,6 +18,7 @@ export default async function Page() {
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <RevenueChart revenue={revenue}/>
+        <LatestInvoices latestInvoices={latestInvoices}/>
       </div>
     </main>
   )
