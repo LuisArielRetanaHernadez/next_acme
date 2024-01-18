d5b5d2e467d076c949f6c071e9b7513d1fbd64c7
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-01-17 18:54:43 -0600

    template de next-vercel

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,10 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
@@ -15,21 +15,18 @@ const config: Config = {
         blue: {
           400: '#2589FE',
           500: '#0070F3',
-          600: '#2F6FEB'
-        }
-      },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+          600: '#2F6FEB',
+        },
       },
     },
     keyframes: {
       shimmer: {
-        '100%': { transform: 'translateX(100%)' }
-      }
-    }
+        '100%': {
+          transform: 'translateX(100%)',
+        },
+      },
+    },
   },
   plugins: [require('@tailwindcss/forms')],
-}
-export default config
+};
+export default config;
