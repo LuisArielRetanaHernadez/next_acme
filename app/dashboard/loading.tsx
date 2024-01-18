8ce868bd5cadc7bab2f7e1a7a208e32eafda56c5
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-01-17 23:55:44 -0600

    agregando un margin auto en el contenedor del loading

--- app/dashboard/loading.tsx
+++ app/dashboard/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center h-screen m-auto">
       <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-red-700" />
     </div>
   )
