9fabebd8ae29fdc05130ab35d1e0a76d7f486f8d
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-01-18 13:11:14 -0600

    agregando el el titulo del page invoices

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,7 +1,11 @@
+import { lusitana } from "@/app/ui/fonts";
+
 export default function Page() {
   return (
-    <div>
-      <h1>Invoices 2</h1>
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+      </div>
     </div>
   )
 
