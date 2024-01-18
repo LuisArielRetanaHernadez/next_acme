37d0941eb20c77dc2a0a033043e92b25faca92a7
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-01-17 22:02:16 -0600

    agregando el componente <SideNav>

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,12 @@
+import SideNav from "../ui/dashboard/sidenav";
+
 export default function Layout ({children}: {children: React.ReactNode}) {
   return (
-    <div>
-      This layout of dashboard
+    <div className="flex h-screen flex-col md:flex-row
+    md:overflow-hidden">
+      <div>
+        <SideNav />
+      </div>
       {children}
     </div>
   )
