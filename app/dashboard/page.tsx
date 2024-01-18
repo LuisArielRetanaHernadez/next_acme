f9c5f038277b0031a5582de2a057b98e302ec81a
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-01-17 23:35:58 -0600

    creando el contenedor donde estan RevenueChart y la LatestInvoices

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -13,6 +13,9 @@ export default async function Page() {
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
 
       </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+
+      </div>
     </main>
   )
 
