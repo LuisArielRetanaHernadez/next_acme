902d0998d3755539b3e48fc093c06f2d42103814
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-01-17 23:37:38 -0600

    agreganod el component RevenueChart

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,5 @@
 import { fetchRevenue } from "../lib/data"
+import RevenueChart from "../ui/dashboard/revenue-chart"
 import { lusitana } from "../ui/fonts"
 
 export default async function Page() {
@@ -14,7 +15,7 @@ export default async function Page() {
 
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-
+        <RevenueChart revenue={revenue}/>
       </div>
     </main>
   )
