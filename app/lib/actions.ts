69658fca2e0b5793b4b4aee9e3e98238e0529b6d
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-01-19 18:14:36 -0600

    creadno el schema createInvoiceSchema con zod

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,5 +1,25 @@
 'use server'
+import { z } from 'zod'
+import { Invoice } from './definitions'
+
+const CreateInvoiceSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['paid', 'unpaid']),
+})
+
+const CreateInvoiceFromSchema = CreateInvoiceSchema.omit({
+  id: true,
+  status: true,
+})
 
 export async function createInvoice(formData: FormData) {
-  console.log(formData)
+  // const rawFormData = { 
+  //   customerId: formData.get('customerId'),
+  //   amount: formData.get('amount'),
+  //   status: formData.get('status'),
+  // }
+
+  const rawFormData = Object.fromEntries(formData.entries())
 }
