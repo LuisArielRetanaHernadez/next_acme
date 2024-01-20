860a9c48c97f40f8a82d6a475158242f8e9c10d1
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-01-19 23:42:58 -0600

    agregando los cards

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,13 +1,21 @@
 import { Suspense } from "react"
-import { fetchLatestInvoices } from "../lib/data"
+import { fetchCardData, fetchLatestInvoices } from "../lib/data"
 import LatestInvoices from "../ui/dashboard/latest-invoices"
 import RevenueChart from "../ui/dashboard/revenue-chart"
 import { lusitana } from "../ui/fonts"
 import { RevenueChartSkeleton } from "../ui/skeletons"
+import { Card } from "../ui/dashboard/cards"
 
 export default async function Page() {
 
   const latestInvoices = await fetchLatestInvoices()
+  const {
+    totalPaidInvoices,
+    totalPendingInvoices,
+    numberOfInvoices,
+    numberOfCustomers,
+  } = await fetchCardData()
+
   console.log(latestInvoices)
   return (
     <main>
@@ -15,6 +23,11 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+
+        <Card title="Collected" value={totalPaidInvoices} type="collected" />
+        <Card title="Pendign" value={totalPendingInvoices} type="pending" />
+        <Card title="Total" value={numberOfInvoices} type="invoices" />
+        <Card title="Collected" value={numberOfCustomers} type="customers" />
 
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
