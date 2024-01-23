94dc6d797cfa77e68981c60274187283b4e0b7a3
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-01-23 15:02:52 -0600

    creando el action server 'updateInvoice'

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -18,6 +18,8 @@ const CreateInvoiceFromSchema = CreateInvoiceSchema.omit({
   date: true,
 })
 
+const updateInvoiceFromSchema = CreateInvoiceSchema.omit({ id: true, formData: FormData})
+
 export async function createInvoice(formData: FormData) {
   const { customerId, amount, status } = CreateInvoiceFromSchema.parse({ 
     customerId: formData.get('customerId'),
@@ -31,6 +33,22 @@ export async function createInvoice(formData: FormData) {
 
   await sql`INSERT INTO invoices (customer_id, amount, status, date)
   VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`
+
+  revalidatePath('/dashboard/invoices')
+  redirect('/dashboard/invoices')
+}
+
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = updateInvoiceFromSchema.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  })
+
+  const amountInCents = amount * 100
+
+  sql`UPDATE invoices SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status} WHERE id = ${id}`
 
   revalidatePath('/dashboard/invoices')
   redirect('/dashboard/invoices')
