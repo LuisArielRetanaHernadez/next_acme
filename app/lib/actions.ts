6101b359b9f6cc455d709ec9b6d7a9d454014384
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-01-23 16:52:03 -0600

    agregando try-catch a los actions

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -21,41 +21,56 @@ const CreateInvoiceFromSchema = CreateInvoiceSchema.omit({
 const updateInvoiceFromSchema = CreateInvoiceSchema.omit({ id: true, date: true})
 
 export async function createInvoice(formData: FormData) {
-  const { customerId, amount, status } = CreateInvoiceFromSchema.parse({ 
-    customerId: formData.get('customerId'),
-    amount: formData.get('amount'),
-    status: formData.get('status'),
-  })
+  try {
+    const { customerId, amount, status } = CreateInvoiceFromSchema.parse({ 
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
+    })
 
-  const amountInCents = amount * 100
+    const amountInCents = amount * 100
 
-  const [date] = new Date().toISOString().split('T')
+    const [date] = new Date().toISOString().split('T')
 
-  await sql`INSERT INTO invoices (customer_id, amount, status, date)
-  VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`
+    await sql`INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`
 
-  revalidatePath('/dashboard/invoices')
-  redirect('/dashboard/invoices')
+    revalidatePath('/dashboard/invoices')
+    redirect('/dashboard/invoices')
+  } catch (error) {
+    console.log(error)
+    throw new Error('Failed to create invoice.')
+  }
 }
 
 
 export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = updateInvoiceFromSchema.parse({
-    customerId: formData.get('customerId'),
-    amount: formData.get('amount'),
-    status: formData.get('status'),
-  })
+  try {
+    const { customerId, amount, status } = updateInvoiceFromSchema.parse({
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
+    })
 
-  const amountInCents = amount * 100
+    const amountInCents = amount * 100
 
-  sql`UPDATE invoices SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status} WHERE id = ${id}`
+    sql`UPDATE invoices SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status} WHERE id = ${id}`
 
-  revalidatePath('/dashboard/invoices')
-  redirect('/dashboard/invoices')
+    revalidatePath('/dashboard/invoices')
+    redirect('/dashboard/invoices')
+  } catch (error) {
+    throw new Error('Failed to update invoice.')
+  }
+
 }
 
 export async function deleteInvoice(id: string) {
-  sql`DELETE FROM invoices WHERE id = ${id}`
-  revalidatePath('/dashboard/invoices')
-  redirect('/dashboard/invoices')
+  try {
+    sql`DELETE FROM invoices WHERE id = ${id}`
+    revalidatePath('/dashboard/invoices')
+    redirect('/dashboard/invoices')
+  } catch (error) {
+    throw new Error('Failed to delete invoice.')
+  }
+
 }
